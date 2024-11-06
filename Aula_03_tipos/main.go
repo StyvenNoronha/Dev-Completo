770b1f33e5d1f2c4145a02dd99541d115be03b85
styven <styvenn16@gmail.com>
2024-11-06 09:09:10 -0300

    aula 1,2,3 em golang

---
+++ Aula_03_tipos/main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Printf("type: %T - Value: %v\n", true, true )
+	fmt.Printf("type: %T - Value: %v\n", "styven", "styven" )
+	fmt.Printf("type: %T - Value: %v\n", 25, 25 )
+	fmt.Printf("type: %T - Value: %v\n", 25.1, 25.1 )
+}
